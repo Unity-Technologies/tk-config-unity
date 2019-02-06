a1ce7af7186c221a4834fc02da112a3690461934
lassond <39058799+lassond@users.noreply.github.com>
2019-02-06 13:37:25 -0500

    UT-1596-Licensing_copyrights_and_third-party_information (#23)
    * Removed copyright headers
    * Added LICENSE.md
    * Removed LICENSE (ADSK)

--- after_project_create.py
+++ after_project_create.py
@@ -1,13 +1,3 @@
-# Copyright (c) 2015 Shotgun Software Inc.
-#
-# CONFIDENTIAL AND PROPRIETARY
-#
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
-# Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
-# not expressly granted therein are reserved by Shotgun Software Inc.
-
 """
 The after_project_create file is executed as part of creating a new project.
 If your starter config needs to create any data in shotgun or do any other
